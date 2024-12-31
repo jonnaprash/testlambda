23d566a6691583c9cd22ee87a083fc166084c02b
jonnaprash <prashjonna@gmail.com>
2024-12-31 10:42:43 +0530

    5

--- src/lamdba_function.py
+++ src/lamdba_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! From GitHub Actions..fix test again!!!!???')
+        'body': json.dumps('Hello from Lambda! From GitHub Actions..fix test again!!!!???pp')
     }
